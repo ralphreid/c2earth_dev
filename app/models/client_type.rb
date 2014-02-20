0a4b054428788277781a117a1397f1ce2494b2f3
Ralph Reid <beresfordjunior@me.com>
2014-02-19 17:52:15 -0800

    updated relationships between client_type, project & phase

--- app/models/client_type.rb
+++ app/models/client_type.rb
@@ -1,3 +1,4 @@
 class ClientType < ActiveRecord::Base
   attr_accessible :client_type
+  belongs_to :phase
 end
--- app/models/phase.rb
+++ app/models/phase.rb
@@ -1,3 +1,5 @@
 class Phase < ActiveRecord::Base
   attr_accessible :Rakefile, :bad_pay_history, :bill_address, :bill_attention, :bill_city, :bill_name, :bill_state, :bill_zip, :company_id, :description, :insurance_notes, :insurance_policy_number, :investigation_type_id, :keywords, :notes, :phase, :project_id, :project_manager_id, :proposal_date, :proposal_number, :report_date, :site_id, :stakeholder_id
+  belongs_to :project
+  has_many :client_types
 end

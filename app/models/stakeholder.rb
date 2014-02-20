0f528b6561d41d309ea8a0311a868734ea928869
Ralph Reid <beresfordjunior@me.com>
2014-02-19 18:00:07 -0800

    updated associations

--- app/models/stakeholder.rb
+++ app/models/stakeholder.rb
@@ -1,4 +1,5 @@
 class Stakeholder < ActiveRecord::Base
   attr_accessible :address, :address_alternate, :cell_phone, :cell_phone_alternate, :city, :city_alternate, :client_type_id, :company_id, :email, :email_alternate, :fax_phone, :fax_phone_alternate, :home_phone, :home_phone_alternate, :name, :name, :notes, :stakeholder_type_id, :state, :state_alternate, :work_phone, :work_phone_alternate, :zip, :zip_alternate
   belongs_to :company
+  has_and_belongs_to_many :stakeholder_types
 end
